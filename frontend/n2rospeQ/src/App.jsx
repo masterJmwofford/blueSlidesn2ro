f89924125292e64e4f0c8cb4853c1a268bd5fa1b
Deli Bodega <deli_bogeda@MacBookAir.hsd1.ga.comcast.net>
2024-11-23 13:56:31 -0500

    initialDesign:

---
+++ frontend/n2rospeQ/src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="yungDolph">
+        <h3 className="n2roMajor fontdiner-swanky-regular">n2rOsPeQ</h3>
+        <img src="../public/n2roMain.jpg" alt="" className="splashMain" />
+
+      </div>
+
+      <div className="navbar">
+        <h3 className="n2roTitle fontdiner-swanky-regular">n2rOsPeQ</h3>
+        <div className="midRange">
+          <p className="ntab">Resources</p>
+          <p className="ntab">Services</p>
+          <p className="ntab">Products</p>
+        </div>
+        <div className="cartContainer">
+          <div className="cartCounter">0</div>
+          <img src="https://icons.veryicon.com/png/o/miscellaneous/flower-mall-color-icon/shopping-cart-114.png" alt="" className="cart" />
+        </div>
+
+      </div>
+
+      <div className="themeContainer">
+
+        <div className="serviceContainer">
+          <h1 className="n2ro fontdiner-swanky-regular">services.</h1>
+          <div className="serviceHolder">
+            <img src="https://wallpapers.com/images/hd/vintage-graffiti-png-mvl-jvsapk2q6lr1l666.jpg" alt="" className="servIcon" />
+            <div className="servtitle fontdiner-swanky-regular">Private Tutorials</div>
+          </div>
+          <div className="serviceHolder">
+            <img src="https://wallpapers.com/images/hd/vintage-graffiti-png-mvl-jvsapk2q6lr1l666.jpg" alt="" className="servIcon" />
+            <div className="servtitle fontdiner-swanky-regular">Night Courses</div>
+          </div>
+          <div className="serviceHolder">
+            <img src="https://wallpapers.com/images/hd/vintage-graffiti-png-mvl-jvsapk2q6lr1l666.jpg" alt="" className="servIcon" />
+            <div className="servtitle fontdiner-swanky-regular">App Development</div>
+          </div>
+          <div className="serviceHolder">
+            <img src="https://wallpapers.com/images/hd/vintage-graffiti-png-mvl-jvsapk2q6lr1l666.jpg" alt="" className="servIcon" />
+            <div className="servtitle fontdiner-swanky-regular">Basic Site Templates</div>
+          </div>
+          <div className="serviceHolder">
+            <img src="https://wallpapers.com/images/hd/vintage-graffiti-png-mvl-jvsapk2q6lr1l666.jpg" alt="" className="servIcon" />
+            <div className="servtitle fontdiner-swanky-regular">Rebranding Packages</div>
+          </div>
+          <div className="serviceHolder">
+            <img src="https://wallpapers.com/images/hd/vintage-graffiti-png-mvl-jvsapk2q6lr1l666.jpg" alt="" className="servIcon" />
+            <div className="servtitle fontdiner-swanky-regular">SneaQr Bots</div>
+          </div>
+        </div>
+
+        <div className="trainingHub">
+          <h1 className="n2ro fontdiner-swanky-regular">training_Hub</h1>
+          <div className="mainVideoScreen"></div>
+          <div className="btnContainer">
+            <img src="https://cdn0.iconfinder.com/data/icons/social-network-9/50/22-512.png" alt="" className="symbol" />
+            <img src="https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png" alt="" className="symbol" />
+            <img src="https://cdn.freebiesupply.com/logos/large/2x/logo-javascript-logo-png-transparent.png" alt="" className="symbol" />
+
+
+
+            <img src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png" alt="" className="symbol" />
+            <img src="https://www.pngall.com/wp-content/uploads/13/Mongodb-PNG-Image-HD.png" alt="https://cdn.prod.website-files.com/609bc2fa29b6d5b7f44a2785/647743f51bc76753239a8bc6_expressjs-logo.webp" className="symbol" />
+            <img src="https://img.icons8.com/fluent/200/express-js.png" alt="" className="symbol" />
+          </div>
+
+        </div>
+
+
+
+        <div className="productContainer">
+          <h1 className="n2ro fontdiner-swanky-regular">products</h1>
+        </div>
+      </div>
+
+      <div className="asideContainer">
+        <h1 className="seven fontdiner-swanky-regular">723</h1>
+        <img src="https://t4.ftcdn.net/jpg/09/65/81/51/360_F_965815159_CVzRXbxsjVQMdDCp4Ryy7Uq53vgTQMuE.jpghttps://t4.ftcdn.net/jpg/09/65/81/51/360_F_965815159_CVzRXbxsjVQMdDCp4Ryy7Uq53vgTQMuE.jpg" alt="" className="scene" />
+      </div>
+
+
+    </>
+
+
+  )
+}
+
+export default App
